c96a68179b2fd86702ead4b708fba8f59b24844c
Cory Ribson <crbi2550@gmail.com>
2015-05-29 10:54:50 -0700

    should probably do a commit about now...

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+var findAndReplace = function(myString, initialWord, updatedWord) {
+  if (myString.search(initialWord) > 1) {
+    return myString.replace(initialWord, updatedWord);
+  } else {
+    return false;
+  }
+}
+
+$(document).ready(function() {
+  var initString;
+  $("form#thisString").submit(function() {
+    initString = $("input#inputString").val();
+    $(".phrase").text(initString);
+    $("#stageTwo").show();
+
+    event.preventDefault();
+  });
+
+  $("#firstClick").click(function() {
+    $("#stageOne").fadeOut();
+  });
+
+  $("form#replaceWordSearch").submit(function() {
+    var oldWord = $("input#find").val();
+    var newWord = $("input#replace").val();
+
+    $(".finalPhrase").text(findAndReplace(initString, oldWord, newWord));
+    $("#stageFinal").show();
+    $("#stageOne").hide();
+
+    event.preventDefault();
+  });
+
+  $("#secondClick").click(function() {
+    $("#stageTwo").fadeOut();
+  });
+
+
+});
---
+++ spec/specs.js
@@ -0,0 +1,12 @@
+describe('findAndReplace', function() {
+  it("will search through a string to find a word", function() {
+    var string = "Let's go to the mall!";
+    var testWord = "base"
+    expect(findAndReplace(string, testWord)).to.equal(false);
+  });
+
+  it("will search through a string, find a word, and replace it", function() {
+    var string = "The bassline is jumping.";
+    expect(findAndReplace(string, "jumping", "pumping")).to.equal("The bassline is pumping.");
+  });
+});
